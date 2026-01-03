801de59932938b547ff8e3158342e9c52da748d7
Hidayatullah <hidayatullah.ofc@gmail.com>
2026-01-03 09:25:16 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menu-toggle");
+const menu = document.querySelector("nav ul");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
